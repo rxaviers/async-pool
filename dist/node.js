304404680fc1884f235d802c88bf3cd827fc7fff
Rafael Xavier de Souza <rxavier@paypal.com>
2018-05-21 18:09:39 -0300

    Add node export

---
+++ dist/node.js
@@ -0,0 +1,10 @@
+var exports;
+var semver = require("semver");
+
+if (semver.satisfies(process.version, "<8")) {
+  exports = require("../lib/es6");
+} else {
+  exports = require("../lib/es7");
+}
+
+module.exports = exports;
