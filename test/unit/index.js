c9d8ff0e4dbdb5f3c84ad72e316c64e791ccfecf
Matthias Hlawatsch <matthias.hlawatsch@jambit.com>
2020-03-13 23:20:44 +0100

    Improve tests for error handling.

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,6 +1,5 @@
 const es6AsyncPool = require("../../lib/es6");
 const es7AsyncPool = require("../../lib/es7");
-const { rejects } = require("assert");
 const { expect } = require("chai");
 
 describe("asyncPool", function() {
@@ -55,40 +54,40 @@ describe("asyncPool", function() {
         expect(results).to.deep.equal([100, 200, 300, 500]);
       });
 
-      it("rejects on error with pool limit effective", async function() {
-        const results = [];
-        const timeout = i =>
-          new Promise((resolve, reject) =>
-            setTimeout(() => {
-              results.push(i);
-
-              if (i === 300) {
-                reject(new Error("Oops"));
-              } else {
-                resolve();
-              }
-            }, i)
-          );
-        await rejects(asyncPool(2, [100, 500, 300, 200], timeout));
+      it("rejects on error (but does not leave unhandled rejections)", async function() {
+        const timeout = _ => Promise.reject();
+        return expect(asyncPool(5, [100, 500, 300, 200], timeout)).to.be.rejected;
+        // check console - no UnhandledPromiseRejectionWarning should appear
       });
 
-      it("rejects on error when pool bigger than needed", async function() {
-        const results = [];
-        const timeout = i =>
-          new Promise((resolve, reject) =>
-            setTimeout(() => {
-              results.push(i);
-
-              if (i === 300) {
-                reject(new Error("Oops"));
-              } else {
-                resolve();
-              }
-            }, i)
+      it("rejects as soon as first promise rejects", async function() {
+        const startedTasks = [];
+        const finishedTasks = [];
+        const timeout = i => {
+          startedTasks.push(i);
+          return new Promise((resolve, reject) =>
+              setTimeout(() => {
+                if (i === 300) {
+                  reject(new Error("Oops"));
+                } else {
+                  finishedTasks.push(i);
+                  resolve();
+                }
+              }, i)
           );
-        await rejects(asyncPool(5, [100, 500, 300, 200], timeout));
+        };
 
-        // check console - no UnhandledPromiseRejectionWarning should appear
+        const testResult = await expect(asyncPool(2, [100, 500, 300, 200], timeout)).to.be.rejected;
+
+        expect(startedTasks).to.deep.equal([100, 500, 300]);
+        expect(finishedTasks).to.deep.equal([100]);
+
+        // tasks started before the error will continue, though - just wait a bit
+        await new Promise(resolve => setTimeout(() => resolve(), 500));
+        expect(startedTasks).to.deep.equal([100, 500, 300]);
+        expect(finishedTasks).to.deep.equal([100, 500]);
+
+        return testResult;
       });
     });
   }
